6ebcc660092a0673953c8f5d72a794a5422d8476
jobtalle <jobtalle@hotmail.com>
2014-10-12 21:16:13 +0200

    Added fancy header

--- include/intx/intx.h
+++ include/intx/intx.h
@@ -1,2 +1,22 @@
+//__________________________________________________________________________________//
+//                                                                                  //
+//                               _       _                                          //
+//                              (_)     | |                                         //
+//                               _ _ __ | |___  __                                  //
+//                              | | '_ \| __\ \/ /                                  //
+//                              | | | | | |_ >  <                                   //
+//                              |_|_| |_|\__/_/\_\ 1.0                              //
+//                                                                                  //
+//                                                                                  //
+//             Copyright (C) 2014 \ Job Talle (jobtalle@hotmail.com)                //
+//__________________________________________________________________________________//
+//                                                                                  //
+//      This program is free software: you can redistribute it and/or modify        //
+//      it under the terms of the 3-clause BSD license.                             //
+//                                                                                  //
+//      You should have received a copy of the 3-clause BSD License along with      //
+//      this program. If not, see <http://opensource.org/licenses/>.                //
+//__________________________________________________________________________________//
+
 #pragma once
 
